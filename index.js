f774265f6be18e712a493f52f948c428601f6eb5
RenatoFernandoSilvaGoncalves <renatogoncalves@unoeste.edu.br>
2024-05-02 23:15:11 -0300

    fix: resp.end();

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use('/listarUsuarios', (req,resp)=>{
     resp.write('</body>');
     resp.write('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>')
     resp.write('</html>');
-
+    resp.end();
 });
 
 app.listen(porta, host, () => {
